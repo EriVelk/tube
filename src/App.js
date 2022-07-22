39038a444b46175e84d97465d6081287ed3edff2
EriVelk <erve@live.com.mx>
2022-07-21 20:25:07 -0500

    Add Home, Menu and Card

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import { useState } from "react";
+import { darkTheme, lightTheme } from "./utils/Theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const Container = styled.div`
+  display: flex;
+`;
 
-`
-
-const Main = styled.div``;
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
     <Container>
-      <Menu/>
-      <Main>
-        <Navbar/>
-      </Main>
+    <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
     </Container>
+    </ThemeProvider>
   );
 }
 
