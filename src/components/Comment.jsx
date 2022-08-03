c4c44565a328152e6063163f82b74dbe550c5340
EriVelk <erve@live.com.mx>
2022-08-03 17:22:59 -0500

    Add Comment, Comments components

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,53 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+  margin: 30px 0px;
+`;
+
+const Avatar = styled.img`
+    width: 50px;
+    height: 50px;
+`;
+
+const Details = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    color: ${({theme}) => theme.text};
+`;
+
+const Name = styled.span`
+    font-size: 13px;
+    font-weight: 500;
+`;
+
+const Date = styled.span`
+    font-size: 12px;
+    font-weight: 400;
+    color: ${({theme}) => theme.textSoft};
+    margin-left: 5px;
+`;
+
+const Text = styled.span`
+    font-size: 14px;
+`;
+
 const Comment = () => {
   return (
-    <div>Comment</div>
+    <Container>
+        <Avatar src="https://cdn-icons-png.flaticon.com/512/190/190670.png"/>
+      <Details>
+        <Name>
+          Erick Veliz <Date>1 dia atras</Date>
+        </Name>
+        <Text>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+        </Text>
+      </Details>
+    </Container>
   )
 }
 
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import styled from "styled-components";
+import Comment from './Comment';
+
+const Container = styled.div`
+`;
+
+const NewComment = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+`;
+
+const Avatar = styled.img`
+    width: 50px;
+    height: 50px;
+`;
+
+const Input = styled.input`
+    border: none;
+    border-bottom: 1px solid ${({theme}) => theme.soft};
+    color: ${({theme}) => theme.text};
+    background-color: transparent;
+    outline: none;
+    padding: 5px;
+    width: 100%;
+`;
 
 const Comments = () => {
   return (
-    <div>Comments</div>
+    <Container>
+    <NewComment>
+        <Avatar src="https://cdn-icons-png.flaticon.com/512/147/147144.png"/>
+        <Input placeholder='Agrega un comentario...'/>
+    </NewComment>
+    <Comment/>
+    <Comment/>
+    <Comment/>
+    <Comment/>
+    </Container>
   )
 }
 
